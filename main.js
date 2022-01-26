4753bf129326df647c67a631040a6e8dd2d77482
Benji <benjamin.bruckmoser@web.de>
2022-01-26 21:10:06 +0100

    Fix some stuff

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ function generateEnemy() {
       let behaviour = behaviours[Math.floor(Math.random() * behaviours.length)];
       let task = tasks[Math.floor(Math.random() * tasks.length)];
       let range = ranges[Math.floor(Math.random() * ranges.length)];
-      resultsElement.innerHTML = `A <b>${threatLevel}</b> threat level enemy that <b>${behaviour}</b> tries to <b>${task}</b> at <b>${range}</b> range.`;
+      resultsElement.innerHTML = `A <b>${threatLevel} threat level</b> enemy that <b>${behaviour}</b> tries to <b>${task}</b> at <b>${range}</b> range.`;
 }
 
 generateEnemy();
